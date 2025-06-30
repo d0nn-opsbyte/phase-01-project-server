df28e9b0b4c19f381b4a2514facb347a96203b91
donald kiarie <donaldkiarie768@gmail.com>
2025-06-30 16:56:31 +0300

    add files

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ const port = process.env.PORT || 3000; //  chose port from here like 8080, 3001
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, '0.0.0.0', () => {
+  console.log(`JSON Server is running on http://localhost:${port}`);
+});
